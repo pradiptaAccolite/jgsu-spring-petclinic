d06c00192ea54aee18c13970babc6396477c9e84
pradipta.roy@accoliteindia.com <pradipta.roy@accoliteindia.com>
2021-07-19 17:24:11 +0530

    duplicate trigger.

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -46,6 +46,7 @@ class ValidatorTests {
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
 		person.setFirstName("");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
